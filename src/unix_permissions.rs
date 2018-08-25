6311536a7c47af7ffc46e41aa7900882ce7d2ef1
Nikolay Volf <nikvolf@gmail.com>
2018-08-25 12:07:02 +0300

    Update unix_permissions.rs

--- src/unix_permissions.rs
+++ src/unix_permissions.rs
@@ -1,6 +1,6 @@
 /// A NOOP struct for bringing the API between Windows and Unix up to parity. To set permissions
 /// properly on Unix, you can just use `std::os::unix::fs::PermissionsExt`.
-pub struct SecurityAttributes
+pub struct SecurityAttributes;
 
 impl SecurityAttributes {
     pub fn empty() -> Self { SecurityAttributes }
