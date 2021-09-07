c1a5f9536fb3540f369a251daf414293f4614a29
Leonardo Razovic <leorazzo@gmail.com>
2021-09-07 09:47:40 +0200

    Run `cargo fmt`

--- examples/client.rs
+++ examples/client.rs
@@ -1,24 +1,33 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use parity_tokio_ipc::Endpoint;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-	let path = std::env::args().nth(1).expect("Run it with server path to connect as argument");
+    let path = std::env::args()
+        .nth(1)
+        .expect("Run it with server path to connect as argument");
 
-	let mut client = Endpoint::connect(&path).await
-		.expect("Failed to connect client.");
+    let mut client = Endpoint::connect(&path)
+        .await
+        .expect("Failed to connect client.");
 
-	loop {
-		let mut buf = [0u8; 4];
-		println!("SEND: PING");
-		client.write_all(b"ping").await.expect("Unable to write message to client");
-		client.read_exact(&mut buf[..]).await.expect("Unable to read buffer");
-		if let Ok("pong") = std::str::from_utf8(&buf[..]) {
-			println!("RECEIVED: PONG");
-		} else {
-			break;
-		}
+    loop {
+        let mut buf = [0u8; 4];
+        println!("SEND: PING");
+        client
+            .write_all(b"ping")
+            .await
+            .expect("Unable to write message to client");
+        client
+            .read_exact(&mut buf[..])
+            .await
+            .expect("Unable to read buffer");
+        if let Ok("pong") = std::str::from_utf8(&buf[..]) {
+            println!("RECEIVED: PONG");
+        } else {
+            break;
+        }
 
-		tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-	}
+        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    }
 }
